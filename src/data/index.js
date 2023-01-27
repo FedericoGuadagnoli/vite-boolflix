6ae91eac9c87a29c13889d145e26b9db83ea8290
FedericoGuadagnoli <guadagnoli1989@gmail.com>
2023-01-27 17:00:38 +0100

    Add language key in index.js

--- src/data/index.js
+++ src/data/index.js
@@ -1,6 +1,7 @@
 const api = {
     key: "d627e336d6a86d4fbabedda81059489d",
-    baseUri: "https://api.themoviedb.org/3"
+    baseUri: "https://api.themoviedb.org/3",
+    language: "IT"
 }
 
 export { api };

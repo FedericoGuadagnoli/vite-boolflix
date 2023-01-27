b0777075c6037116090ad6ed39259a759e03c43e
FedericoGuadagnoli <guadagnoli1989@gmail.com>
2023-01-27 17:09:04 +0100

    Create component MovieList.vue

---
+++ src/components/Movie/MovieList.vue
@@ -0,0 +1,23 @@
+<script>
+import { store } from '../../data/store';
+export default {
+    name: 'MovieList',
+    data() {
+        return {
+            store
+        }
+    }
+}
+</script>
+
+<template>
+    <section id="movies" class="container my-5">
+        <div class="row g-4 row-cols-2 row-cols-md-3 row-cols-lg-4">
+
+        </div>
+    </section>
+</template>
+
+<style>
+
+</style>

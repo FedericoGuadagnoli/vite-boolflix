1e1ffb6423a75f21fcf2109bc193dfbd966a6561
FedericoGuadagnoli <guadagnoli1989@gmail.com>
2023-01-27 16:16:29 +0100

    Import SearchForm in App

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
 <script>
-
+import SearchForm from './components/SearchForm.vue';
+export default {
+  components: { SearchForm },
+  data() {
+    return {
+      filterMovie: ''
+    }
+  },
+  methods: {
+    OnTermChange(term) {
+      this.filterMovie = term;
+    }
+  }
+}
 </script>
 
 <template>
-
+  <search-form @term-change="OnTermChange" placeholder="Scrivi il nome di un film"></search-form>
 </template>
 
 <style scoped lang="scss">

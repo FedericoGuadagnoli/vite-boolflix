575746bf3848b054f85c73d52b329892b325890e
FedericoGuadagnoli <guadagnoli1989@gmail.com>
2023-01-27 20:16:56 +0100

    Create component ProductionCard

---
+++ src/components/Production/ProductionCard.vue
@@ -0,0 +1,40 @@
+<script>
+export default {
+    name: 'ProductionCard',
+    props: {
+        item: Object
+    },
+    computed: {
+        title() {
+            return this.item.title || this.item.name;
+        },
+        originalTitle() {
+            return this.item.original_title || this.item.original_name;
+        },
+        hasFlag() {
+            const flags = ['it', 'en'];
+            return flags.includes(this.item.original_language);
+        },
+        flagSrc() {
+            const url = new URL(`../../assets/img/${this.item.original_language}.png`, import.meta.url);
+            return url.href;
+        }
+    }
+};
+</script>
+
+<template>
+    <div class="production-card text-center p-2 text-dark">
+        <h2>{{ title }}</h2>
+        <h2>{{ originalTitle }}</h2>
+        <div>
+            <img class="img-fluid" v-if="hasFlag" :src="flagSrc" :alt="item.original_language">
+            <h3 v-else>{{ item.original_language }}</h3>
+        </div>
+        <h3>{{ item.vote_average }}</h3>
+    </div>
+</template>
+
+<style>
+
+</style>

f178af578ac6b3dcb8b6cadc85adc9e81b0b79ea
FedericoGuadagnoli <guadagnoli1989@gmail.com>
2023-01-27 16:15:52 +0100

    Create component SearchForm.vue

---
+++ src/components/SearchForm.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+    name: 'SearchForm',
+    data() {
+        return {
+            term: ''
+        }
+    },
+    props: {
+        placeholder: String
+    },
+    emits: ['term-change', 'form-submit']
+}
+</script>
+
+<template>
+    <form @submit.prevent="$emit('form-submit')">
+        <input type="text" v-model.trim="term" class="form-control flex-grow-3" :placeholder="placeholder"
+            @keyup="$emit('term-change', term)">
+        <button class="btn btn-primary">Cerca</button>
+    </form>
+</template>
+
+<style>
+
+</style>

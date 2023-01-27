7e3d40b5c5d67a6cc0581b5562b9405a24db9cd0
FedericoGuadagnoli <guadagnoli1989@gmail.com>
2023-01-27 17:01:52 +0100

    Import store and do call API

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,48 @@
 <script>
+import axios from 'axios';
 import SearchForm from './components/SearchForm.vue';
+import { api } from './data';
+import { store } from './data/store';
 export default {
+  name: 'Boolfix',
   components: { SearchForm },
   data() {
     return {
-      filterMovie: ''
+      filterMovie: '',
+      store
     }
   },
+  computed: {
+    axiosConfig() {
+      const { key, language } = api;
+      return {
+        params: {
+          api_key: key,
+          language: language,
+          query: this.filterMovie
+        }
+      }
+    },
+  },
   methods: {
-    OnTermChange(term) {
+    onTermChange(term) {
       this.filterMovie = term;
+    },
+    searchMovies() {
+      axios.get(`${api.baseUri}/search/movie`, this.axiosConfig)
+        .then((res) => {
+          store.movies = res.data.results
+        }).catch((error) => {
+          console.error(error)
+        });
     }
   }
-}
+};
 </script>
 
 <template>
-  <search-form @term-change="OnTermChange" placeholder="Scrivi il nome di un film"></search-form>
+  <search-form @term-change="onTermChange" @form-submit="searchMovies"
+    placeholder="Scrivi il nome di un film"></search-form>
 </template>
 
 <style scoped lang="scss">
